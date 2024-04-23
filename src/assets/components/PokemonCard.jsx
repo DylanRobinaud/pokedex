57f2fcb187c4944f6affabe74a06f9659517fc81
Dylan Robinaud <dylan.rbnd@gmail.com>
2024-04-23 15:23:41 +0200

    Création carte pokemon(Bulbizarre)

---
+++ src/assets/components/PokemonCard.jsx
@@ -0,0 +1,15 @@
+function PokemonCard() {
+  return (
+    <>
+      <figure>
+        <img
+          src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
+          alt="Cette image représente le Pokémon Bulbizare."
+        />
+        <figcaption>Bulbizare</figcaption>
+      </figure>
+    </>
+  );
+}
+
+export default PokemonCard;
