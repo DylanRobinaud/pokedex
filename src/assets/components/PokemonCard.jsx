426a5a101ec8359ea4e7cf29309249386e47fd90
Dylan Robinaud <dylan.rbnd@gmail.com>
2024-04-24 16:37:04 +0200

    Création props types

--- src/assets/components/PokemonCard.jsx
+++ src/assets/components/PokemonCard.jsx
@@ -15,4 +15,11 @@ function PokemonCard({ pokemon }) {
   );
 }
 
+PokemonCard.propTypes = {
+  pokemon: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    imgSrc: PropTypes.string,
+  }).isRequired,
+};
+
 export default PokemonCard;
